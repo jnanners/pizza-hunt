0f89a9e4f52e4644a89a98075e88964051821d1e
JBanana <johnandrewharris3@gmail.com>
2021-12-07 11:57:41 -0600

    comments and pizza list

--- public/assets/js/pizza-list.js
+++ public/assets/js/pizza-list.js
@@ -1,5 +1,17 @@
 const $pizzaList = document.querySelector('#pizza-list');
 
+//get pizza list from api, convert to json, run each one through printPizza function
+const getPizzaList = () => {
+  fetch("/api/pizzas")
+  .then(response => response.json())
+  .then(pizzaListArr => {
+    pizzaListArr.forEach(printPizza);
+  })
+  .catch(err => {
+    console.log(err);
+  });
+};
+
 const printPizza = ({ _id, pizzaName, toppings, size, commentCount, createdBy, createdAt }) => {
   const pizzaCard = `
     <div class="col-12 col-lg-6 flex-row">
@@ -26,3 +38,6 @@ const printPizza = ({ _id, pizzaName, toppings, size, commentCount, createdBy, c
 
   $pizzaList.innerHTML += pizzaCard;
 };
+
+//call function on load
+getPizzaList();
